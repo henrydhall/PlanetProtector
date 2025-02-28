aec02a11545e4acd393b97866ed23f60fddf185a
HenrydHall <henrydhall3.14@gmail.com>
2025-02-28 16:54:33 -0700

    added laser, orbiting works, linting

--- planet_protector/planet_protector.py
+++ planet_protector/planet_protector.py
@@ -14,22 +14,23 @@ if not pg.image.get_extended():
 
 # Pygame setup
 HEIGHT = 600
-WIDTH = 600
-DISPLAY_SURFACE = pg.display.set_mode((HEIGHT,WIDTH))
+WIDTH = 700
+DISPLAY_SURFACE = pg.display.set_mode((WIDTH, HEIGHT))
 FPS = pg.time.Clock()
 COLOR_BACKGROUD = (0, 0, 0)
-ACCELERATION = .5
+ACCELERATION = 0.1
 
 # Globals
-FRAME_RATE = 4
+FRAME_RATE = 15
 
 
 class Planet(pg.sprite.Sprite):
     """Planet."""
+
     def __init__(self):
         self.image = pg.image.load('resources/planet.png')
         self.rect = self.image.get_rect()
-        self.rect.center = (250,250)
+        self.rect.center = (250, 250)
 
     def update(self):
         """Planet update method."""
@@ -42,13 +43,16 @@ class Planet(pg.sprite.Sprite):
 
 class Meteor(pg.sprite.Sprite):
     """Meteor class"""
-    x_v = 10
+
+    # TODO: I'd like to have a set speed, and just have that come towards the planet.
+    # TODO: set random x/y away from planet to start.
+    x_v = 0
     y_v = 0
 
     def __init__(self):
         self.image = pg.image.load('resources/meteor_1.png')
         self.rect = self.image.get_rect()
-        self.rect.center = (251,50)
+        self.rect.center = (250, 50)
 
     def draw(self, surface):
         """Draw the meteor"""
@@ -56,18 +60,20 @@ class Meteor(pg.sprite.Sprite):
 
     def update(self, planet: Planet):
         """Update our x and y velocities, move the meteor."""
-        #self.x_v, self.y_v = self.calculate_acceleration(planet)
         self.calculate_acceleration(planet)
-        self.rect.move_ip(self.x_v,self.y_v)
+        self.rect.move_ip(self.x_v, self.y_v)
 
     def calculate_acceleration(self, planet: Planet) -> tuple[float, float]:
-        """Calculate the component accelerations.
-        TODO: fix the division by zero error when x or y matches the planet's.
-        """
-        angle = math.atan( (self.rect.centerx - planet.rect.centerx) / (self.rect.centery-planet.rect.centery) )
+        """Calculate the component accelerations."""
+        if self.rect.centery == planet.rect.centery:
+            if self.rect.centerx < planet.rect.centerx:
+                angle = math.pi / 2
+            else:
+                angle = -math.pi / 2
+        else:
+            angle = math.atan((self.rect.centerx - planet.rect.centerx) / (self.rect.centery - planet.rect.centery))
         opposite = abs(math.sin(angle)) * ACCELERATION
         adjacent = abs(math.cos(angle)) * ACCELERATION
-        print(opposite, adjacent, self.x_v, self.y_v, self.rect.x, self.rect.y)
         if self.rect.centerx > planet.rect.centerx:
             self.x_v -= opposite
         else:
@@ -78,12 +84,26 @@ class Meteor(pg.sprite.Sprite):
         else:
             self.y_v += adjacent
 
+
+class Laser(pg.sprite.Sprite):
+    """Laser sprite to reduce meteor mass."""
+
+    def __init__(self):
+        self._color = (255, 0, 0)
+
+    def draw(self, planet: Planet, meteor: Meteor):
+        """Draw the laser."""
+        pg.draw.line(DISPLAY_SURFACE, (255, 0, 0), planet.rect.center, meteor.rect.center, 3)
+
+
 def main():
     """
     Main game loop.
     """
     planet = Planet()
     meteor1 = Meteor()
+    laser1 = Laser()
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -91,12 +111,14 @@ def main():
                 sys.exit()
         planet.draw(DISPLAY_SURFACE)
         meteor1.draw(DISPLAY_SURFACE)
+        laser1.draw(planet, meteor1)
 
         meteor1.update(planet)
         pg.display.update()
 
         DISPLAY_SURFACE.fill(COLOR_BACKGROUD)
         FPS.tick(FRAME_RATE)
+
 
 if __name__ == '__main__':
     main()
