9a60dab7a1bb0c393322bb8d4957a8bde4741e65
HenrydHall <henrydhall3.14@gmail.com>
2025-02-27 20:40:19 -0700

    fixed x velocity bug

--- planet_protector/planet_protector.py
+++ planet_protector/planet_protector.py
@@ -13,15 +13,15 @@ if not pg.image.get_extended():
     raise SystemExit("Extended image module required.")
 
 # Pygame setup
-HEIGHT = 500
-WIDTH = 500
+HEIGHT = 600
+WIDTH = 600
 DISPLAY_SURFACE = pg.display.set_mode((HEIGHT,WIDTH))
 FPS = pg.time.Clock()
 COLOR_BACKGROUD = (0, 0, 0)
-ACCELERATION = .2
+ACCELERATION = .5
 
 # Globals
-FRAME_RATE = 30
+FRAME_RATE = 4
 
 
 class Planet(pg.sprite.Sprite):
@@ -42,13 +42,13 @@ class Planet(pg.sprite.Sprite):
 
 class Meteor(pg.sprite.Sprite):
     """Meteor class"""
-    x_v = 2
+    x_v = 10
     y_v = 0
 
     def __init__(self):
         self.image = pg.image.load('resources/meteor_1.png')
         self.rect = self.image.get_rect()
-        self.rect.center = (25,251)
+        self.rect.center = (251,50)
 
     def draw(self, surface):
         """Draw the meteor"""
@@ -62,15 +62,16 @@ class Meteor(pg.sprite.Sprite):
 
     def calculate_acceleration(self, planet: Planet) -> tuple[float, float]:
         """Calculate the component accelerations.
-        TODO: account for the opposite and adjacent changing x or y depending on which quadrant we're in.
+        TODO: fix the division by zero error when x or y matches the planet's.
         """
         angle = math.atan( (self.rect.centerx - planet.rect.centerx) / (self.rect.centery-planet.rect.centery) )
-        opposite = math.sin(angle) * ACCELERATION
-        adjacent = math.cos(angle) * ACCELERATION
+        opposite = abs(math.sin(angle)) * ACCELERATION
+        adjacent = abs(math.cos(angle)) * ACCELERATION
+        print(opposite, adjacent, self.x_v, self.y_v, self.rect.x, self.rect.y)
         if self.rect.centerx > planet.rect.centerx:
-            self.x_v += opposite
-        else:
             self.x_v -= opposite
+        else:
+            self.x_v += opposite
 
         if self.rect.centery > planet.rect.centerx:
             self.y_v -= adjacent
